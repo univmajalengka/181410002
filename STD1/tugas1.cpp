d61fa9f966beddb6df51026cf6efb8b3429a76f8
harissakurudin <harissakurudin@gmail.com>
2019-05-10 19:52:23 +0700

    upload tugas

---
+++ STD1/tugas1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+char kendaraan [7][15] =
+{"sepeda","beca","bajai","motor","mobil","busway","bus"};
+int n;
+int main()
+{
+	for(n=0;n<7;n++)
+	{
+	cout <<"masukan salah satu kendaraan : ";
+	cin>> n;
+	cout <<"nama kendaraan tersebut adalah :" <<kendaraan[n];
+	cout << "\n";
+	}
+	return 0;
+}
